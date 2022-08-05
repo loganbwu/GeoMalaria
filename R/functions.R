b441160f76465b49eb52ef12c796b4c6f07f60b6
Logan Wu <loganbwu@gmail.com>
2022-08-05 15:09:01 +1000

    Add function to change mosquito density raster

--- R/functions.R
+++ R/functions.R
@@ -10,7 +10,7 @@ make_perlin_mosquitoes = function(env_dimensions) {
   mosquito_raster = raster(vals=0, nrows=100, ncols=100,
                               xmn=0, xmx=env_dimensions[1],
                               ymn=0, ymx=env_dimensions[2],
-                              crs=NA)#"+units=km")
+                              crs="+units=km")
   noise = noise_perlin(dim(mosquito_raster)[1:2])
   mosquito_raster[] = (noise - min(noise)) * 10000
   names(mosquito_raster) = "count"
