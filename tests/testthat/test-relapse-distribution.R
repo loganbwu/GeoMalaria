6b090b63e2bc720654e06b62326acd147a154d99
Logan Wu <loganbwu@gmail.com>
2022-09-20 17:12:29 +1000

    Check that distribution of relapse delays doesn't depend on which number relapse it is

--- tests/testthat/test-relapse-distribution.R
+++ tests/testthat/test-relapse-distribution.R
@@ -22,30 +22,63 @@ test_that("repeated relapse behaviour is as expected", {
     ymn = -1,
     ymx = 1)
   
+  p_relapse = 0.4
   sim = Simulation$new(humans = people,
                        locations = houses,
                        mosquito_raster = mosquito_raster,
                        duration_human_infectivity = 0.1,
                        bite_rate = 0.1,
                        mosquito_death_rate = 0.5,
-                       p_relapse = 0.5,
+                       p_relapse = p_relapse,
                        mean_recovery = 14)
   
-  # Simulate both for the same total time period
-  end_t = 25
-  diff = pbapply::pbsapply(1:100, function(x) {
-    sim_0.5 = sim$clone()
-    sim_1 = sim$clone()
-    while (sim_0.5$t <= end_t) {
-      sim_0.5$iterate(0.75)
-    }
-    while (sim_1$t <= end_t) {
-      sim_1$iterate(1)
-    }
-    nrow(sim_1$linelist) - nrow(sim_0.5$linelist)
-  })
-  diff_interval = quantile(diff, c(0.025, 0.975))
+  for (i in 1:1000) {
+    sim$iterate(1)
+  }
   
-  # Expect most of the differences in outbreak size are around 0
-  expect_true(diff_interval[1] <= 0 & diff_interval[2] >= 0)
+  # Check that relapse times make sense
+  relapses = sim$linelist %>%
+    group_by(ID) %>%
+    mutate(ix = row_number() - 1,
+           delay = t_infection - lag(t_infection)) %>%
+    slice(-1)
+  
+  # ggplot(relapses, aes(x=t_infection)) +
+  #   geom_density() +
+  #   facet_wrap(vars(n))
+  # 
+  ggplot(relapses, aes(x=delay)) +
+    geom_density() +
+    facet_wrap(vars(ix))
+  
+  # Check the distribution of relapse delays
+  distributions = relapses %>%
+    group_by(ix) %>%
+    summarise(mean_delay = mean(delay),
+              sd = sd(delay),
+              n = n())
+  means = with(
+    distributions,
+    list(mean_delay = sum(mean_delay * n) / sum(n),
+         mean_sd = sum(sd * n) / sum(n))
+  )
+  distributions = distributions[distributions$n > 100,]
+  differences = list(
+    mean_delay = abs(distributions$mean_delay - means$mean_delay),
+    mean_sd = abs(distributions$sd - means$mean_sd))
+  # All should be roughly similar
+  expect_true(all(differences$mean_delay < 5) & all(differences$mean_sd < 5))
+  
+  
+  # ggplot(relapses, aes(x = n)) +
+  #   geom_bar()
+  
+  n_relapses = tibble(n = table(relapses$ix)) %>%
+    mutate(frac = n / lag(n)) %>%
+    drop_na()
+  # Investigate the number of relapses that result in subsequent relapses
+  weighted_p = with(n_relapses, sum(n*frac) / sum(n))
+  
+  # Should be roughly similar
+  expect_equal(weighted_p, p_relapse, tolerance=0.2)
 })
