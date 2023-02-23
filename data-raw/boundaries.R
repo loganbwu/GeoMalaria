abd35cbc99d4780d678487953c41995052c3dba0
Logan Wu <loganbwu@gmail.com>
2023-02-23 15:30:03 +0800

    Add boundary dataset

---
+++ data-raw/boundaries.R
@@ -0,0 +1,12 @@
+## code to prepare boundary datasets goes here
+
+library(sf)
+adm0 = st_read("https://github.com/wmgeolab/geoBoundaries/raw/df46a8320bb2f703640664510273bec2be88b180/releaseData/gbOpen/THA/ADM0/geoBoundaries-THA-ADM0_simplified.geojson")
+adm1 = st_read("https://github.com/wmgeolab/geoBoundaries/raw/eb8c046c824d34547fbe9a50b7308b443e76c106/releaseData/gbOpen/THA/ADM1/geoBoundaries-THA-ADM1_simplified.geojson")
+adm2 = st_read("https://github.com/wmgeolab/geoBoundaries/raw/922e33de54786eed936fc13b7b3fd28c50189d5b/releaseData/gbOpen/THA/ADM2/geoBoundaries-THA-ADM2_simplified.geojson")
+
+thailand = adm0
+tak = adm1[adm1$shapeName == "Tak Province",]
+thasongyang = adm2[adm2$shapeName == "Tha Song Yang",]
+
+usethis::use_data(thailand, tak, thasongyang, internal = TRUE)
